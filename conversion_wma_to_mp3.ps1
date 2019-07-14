c7369cc60c5a304c3f338584c9d7328a89b8f676
DESKTOP <heimana@manuia-geek.com>
2019-07-13 22:36:13 -1000

    first commit

---
+++ conversion_wma_to_mp3.ps1
@@ -0,0 +1,21 @@
+﻿# init
+$ffmpeg_exe_path = "D:\ffmpeg\bin\ffmpeg.exe"
+$folder_wma = "F:\_mp3_yt\tmp2"
+$folder_mp3 = "F:\_mp3_yt\done\"
+
+#list child
+$wma_files = Get-ChildItem -Path $folder_wma 
+
+ForEach ($wma_file in $wma_files){
+    
+    #paths
+    $path_file_source = $wma_file.FullName    
+    $path_file_dest = "{0}{1}{2}" -f $folder_mp3,$wma_file.Basename,".mp3"
+
+    #exec ligne
+    $line_exec = '{0} -i "{1}" "{2}"' -f $ffmpeg_exe_path,$wma_file.FullName,$path_file_dest    
+    
+    #lancement 
+    Write-Host $line_exec
+    Invoke-Expression $line_exec
+}
---
+++ extract audio from video_to_mp3.ps1
@@ -0,0 +1,21 @@
+﻿# init
+$ffmpeg_exe_path = "D:\ffmpeg\bin\ffmpeg.exe"
+$folder_video = "F:\_mp3_yt\ytaudioextract\"
+$folder_audio = "F:\_mp3_yt\ytaudioextract\done\"
+
+#list child
+$video_files = Get-ChildItem -Path $folder_video 
+
+ForEach ($video_file in $video_files){
+    
+    #paths
+    $path_file_source = $video_file.FullName    
+    $path_file_dest = "{0}{1}{2}" -f $folder_audio,$video_file.Basename,".mp3"
+
+    #exec ligne
+    $line_exec = '{0} -i "{1}" -vn "{2}"' -f $ffmpeg_exe_path,$video_file.FullName,$path_file_dest    
+    
+    #lancement 
+    Write-Host $line_exec
+    Invoke-Expression $line_exec
+}
